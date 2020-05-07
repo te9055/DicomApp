38ee259f74ab45ea542b8961d2d7f3b2dcfb8828
Tom Edwards <edwardsai@cardiff.ac.uk>
2020-05-07 15:41:48 +0100

    work towards dmc conversion

--- src/main/java/com/sample/SelectDicomServlet.java
+++ src/main/java/com/sample/SelectDicomServlet.java
@@ -1,22 +1,18 @@
 package com.sample;
 
-import com.sample.model.DicomType;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
-import javax.servlet.ServletResponse;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -24,11 +20,30 @@ import javax.servlet.annotation.WebServlet;
 
 
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.dcm4che2.imageio.plugins.dcm.DicomImageReadParam;
+
+
+
+
 @WebServlet(
         name = "selectdicomservlet",
         urlPatterns = "/UploadDicom"
 )
+
 public class SelectDicomServlet extends HttpServlet {
+
+    static BufferedImage myJpegImage=null;
 
     private static final long serialVersionUID = 1 ;
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -37,6 +52,8 @@ public class SelectDicomServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String file_name = null;
         String fileName = "C:\\Servers\\apache-tomcat-9.0.34\\webapps\\DicomApp_war\\fileupload\\tmpfile.jpg";
+        String outName = "C:\\Servers\\apache-tomcat-9.0.34\\webapps\\DicomApp_war\\fileupload\\tmpfile.jpg";
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         boolean isMultipartContent = ServletFileUpload.isMultipartContent(request);
@@ -46,7 +63,7 @@ public class SelectDicomServlet extends HttpServlet {
         FileItemFactory factory = new DiskFileItemFactory();
         ServletFileUpload upload = new ServletFileUpload(factory);
         try {
-            List < FileItem > fields = upload.parseRequest(request);
+            List <FileItem> fields = upload.parseRequest(request);
             Iterator < FileItem > it = fields.iterator();
             if (!it.hasNext()) {
                 return;
@@ -66,7 +83,34 @@ public class SelectDicomServlet extends HttpServlet {
                         fileItem.write(new File(fileName));
                     }
                 }
+                File dicomimage = new File("d:\\dicom\\in.dcm");
+
+                BufferedImage outputjpg = null;
+                Iterator<ImageReader> iter = ImageIO.getImageReadersByFormatName("DICOM");
+
+                ImageReader reader =  (ImageReader) iter.next();
+
+                DicomImageReadParam param  = (DicomImageReadParam) reader.getDefaultReadParam();
+
+                try{
+                    ImageInputStream iis = ImageIO.createImageInputStream(dicomimage);
+                    reader.setInput(iis,false);
+                    outputjpg = reader.read(0,param);
+                    iis.close();
+                    if(outputjpg == null){
+                        System.out.println("error with DICOM");
+                        return;
+                    }
+                    File myJpegFile = new File("d:\\dicom\\out.jpg");
+                    OutputStream output = new BufferedOutputStream(new FileOutputStream(myJpegFile));
+                    ImageIO.write(myJpegImage,"jpeg",output);
+                    output.close();
+                }
+                catch (IOException e){
+                    System.out.println("error with DICOM");
+                }
             }
+
         } catch (Exception e) {
             final String dir = System.getProperty("user.dir");
             System.out.println("current dir = " + dir);
